cb0147c9ab3ccf75fc5308526f304006fda5526c
Sinisa <sinisha.stojanovic@gmail.com>
2022-05-28 00:37:43 +0200

    Started with Header component

---
+++ src/Header/Header.js
@@ -0,0 +1,13 @@
+import classes from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <div className={classes.header}>
+      <div className={classes.header_container}>
+        <h1>Header</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
