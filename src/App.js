e5a1c09ac1f887601245118df2a3b177d1d0bbb5
Sinisa <sinisha.stojanovic@gmail.com>
2022-05-28 16:03:20 +0200

    APP_DATA added to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,72 @@ import "./App.css";
 import Header from "./Header/Header";
 import { Fragment } from "react";
 
+const APP_DATA = {
+  views: {
+    title: "Views",
+    trendViewsTitle: "Total Views",
+    numberOfViews: "386,200",
+    percentageOfViews: 98,
+    timeFrameBadgeColor: {
+      primary: {
+        bckColor: "rgb(222, 243, 184)",
+        textColor: "rgb(107, 146, 44)",
+        description: "Monthly",
+      },
+      overMouse: {
+        bckColorHover: "rgb(182,234,87)",
+        textColorHover: "rgb(47,72,7)",
+        descriptionHover: "Past Month",
+      },
+    },
+  },
+
+  orders: {
+    title: "Orders",
+    trendOrdersTitle: "Total Orders",
+    numberOfOrders: "80,800",
+    percentageOfOrders: 28,
+    timeFrameBadgeColor: {
+      primary: {
+        bckColor: "rgb(250, 222, 162)",
+        textColor: "rgb(241, 157, 69)",
+        description: "Annual",
+      },
+      overMouse: {
+        bckColorHover: "rgb(254,128,25)",
+        textColorHover: "rgb(255,253,249)",
+        descriptionHover: "Last Year",
+      },
+    },
+  },
+
+  visited: {
+    title: "Visited",
+    timeFrameBadgeColor: {
+      primary: {
+        bckColor: "rgb(240,227,253)",
+        textColor: "rgb(97,0,231)",
+        description: "Today",
+      },
+      overMouse: {
+        bckColorHover: "rgb(90,0,230)",
+        textColorHover: "rgb(255,253,249)",
+        descriptionHover: "Yesterday",
+      },
+    },
+    rapidPace: {
+      trendRapidTitle: "Rapid Pace",
+      numberOfRapid: "406,42",
+      percentageOfRapid: 23,
+    },
+    slowPace: {
+      trendSlowTitle: "Slow Pace",
+      numberOfSlow: "206,12",
+      percentageOfSlow: 1.58,
+    },
+  },
+};
+
 function App() {
   return (
     <Fragment>
